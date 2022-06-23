6e1978dd0c81fa79319ec901442a49f7ed190465
Serhii Vatulyev <4845637@gmail.com>
2022-06-23 12:42:36 +0300

    6 kyu :)

---
+++ 6-kyu/Replace-With-Alphabet-Position.js
@@ -0,0 +1,26 @@
+function alphabetPosition(text) {
+    let arr = text.split('')
+    let result = []
+    for (let l in arr){
+        if(arr[l].match(/[A-Za-z]/i)) result.push(arr[l].toLowerCase().charCodeAt(0) - 96)
+    }
+    return result.join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The narwhal bacons at midnight.")); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+
+/*
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+https://www.codewars.com/kata/546f922b54af40e1e90001da
+*/
